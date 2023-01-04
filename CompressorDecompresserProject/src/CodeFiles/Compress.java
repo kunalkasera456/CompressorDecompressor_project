4680b5d8059c5dee5a99cfb27e82f6d9696cc89e
kunal kumar <105341082+kunalkasera456@users.noreply.github.com>
2023-01-04 22:11:23 +0530

    initial commit

---
+++ CompressorDecompresserProject/src/CodeFiles/Compress.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CodeFiles;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ *
+ * @author kunal
+ */
+public class Compress {
+    
+     public static void compressFunction(File file) throws FileNotFoundException, IOException
+     {
+        String fileDirectory = file.getParent();
+        System .out.println(fileDirectory);
+        FileInputStream fis = new FileInputStream(file);
+        FileOutputStream fos = new FileOutputStream(fileDirectory + "/CompressedFile.gz");
+        GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
+        
+        byte[] buffer = new byte[1024];
+        int len;
+        
+        while((len=fis.read(buffer))!= -1)
+        {
+            gzipOS.write(buffer,0, len);
+        }
+        
+        gzipOS.close();
+        fos.close();
+        fis.close();
+     }  
+     public static void main(String[] args) throws IOException{
+        File path = new File("C:\\Users\\kunal\\OneDrive\\Documents\\NetBeansProjects\\CompressorDecompresserProject\\src\\CodeFiles\\CompressFile.txt");
+        
+        
+        compressFunction(path);
+    }
+    
+}
